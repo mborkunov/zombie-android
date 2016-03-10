389841795794fb3914fbc729439484dc767cd31f
Maxim Borkunov <maxim.borkunov@gmail.com>
2016-03-10 23:44:55 +0300

    indicator image added. some constants

---
+++ core/src/main/scala/com/jelastic/energy/zombie/core/Constants.scala
@@ -0,0 +1,10 @@
+package com.jelastic.energy.zombie.core
+
+object Constants {
+
+  val PI = Math.PI
+  val DOUBLE_PI = Math.PI * 2
+  val HALF_PI = Math.PI / 2
+  val THREE_HALF_PI = HALF_PI * 3
+
+}
